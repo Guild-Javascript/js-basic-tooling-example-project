652f42f6d720871fad27bec92a2b3565ec191765
bcooperz1995 <brandon.cooper@mrisoftware.com>
2021-08-11 14:42:52 +1000

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import { ProjectList } from './App/ProjectList';
+
+class App {
+  static init() {
+    const activeProjectsList = new ProjectList('active');
+    const finishedProjectsList = new ProjectList('finished');
+
+    activeProjectsList.setSwitchHandlerFunction(
+      finishedProjectsList.addProject.bind(finishedProjectsList)
+    );
+    finishedProjectsList.setSwitchHandlerFunction(
+      activeProjectsList.addProject.bind(activeProjectsList)
+    );
+  }
+}
+
+App.init();
